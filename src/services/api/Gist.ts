c5c87cc4fac42826e4eb88dea8185afc360ad3af
Umair Syed <itsumairsyed@gmail.com>
2023-11-13 18:02:50 +0500

    Grid view with infinite scroll completed

--- src/services/api/Gist.ts
+++ src/services/api/Gist.ts
@@ -1,4 +1,8 @@
-import { PublicGistsQueryKey } from "../../types/Gist.t";
+import config from "../../config";
+import {
+  PublicGistsQueryKey,
+  PublicSingleGistQueryKey,
+} from "../../types/Gist.t";
 import { axiosInstance } from "./config";
 import { QueryFunctionContext } from "@tanstack/react-query";
 
@@ -6,4 +10,14 @@ export function getPublicGistsApi({
   queryKey: [, { page, limit }],
 }: QueryFunctionContext<PublicGistsQueryKey>) {
   return axiosInstance.get(`/gists/public?page=${page}&per_page=${limit}`);
+}
+
+export function getSinglePublicGistApi({
+  queryKey: [, { gistId }],
+}: QueryFunctionContext<PublicSingleGistQueryKey>) {
+  return axiosInstance.get(`/gists/${gistId}`, {
+    headers: {
+      Authorization: `Bearer ${config.GITHUB_PERSONAL_ACCESS_TOKEN}`
+    }
+  });
 }
--- src/services/api/config.ts
+++ src/services/api/config.ts
@@ -10,5 +10,6 @@ export const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(function (config) {
   config.headers["Content-Type"] = "application/json";
   config.headers["X-GitHub-Api-Version"] = "2022-11-28";
+  config.headers["Accept"] = "application/vnd.github+json";
   return config;
 });
